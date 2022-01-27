41b64e6637aa224c5f137890005ff62f1c28fb53
Asmith18 <Smittysadam@gmail.com>
2022-01-27 11:57:11 -0600

    initial commit

---
+++ SmartHome/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SmartHome
+//
+//  Created by adam smith on 1/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
